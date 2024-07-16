e972302ed88db71c5432d324fe5f3d81e5a7fc48
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2024-07-16 15:25:23 -0300

    setup cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = require("./routes");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
+
+const corsOptions = {
+	origin: process.env.VUE_APP_FRONTEND_URL, // Ensure this is correct
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	credentials: true,
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
